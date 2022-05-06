a9a2d09c3205ed6cb70963c29d3eb554a3693d32
Ismael Balafrej <ismael.balafrej@usherbrooke.ca>
2022-05-06 15:35:38 +0200

    New version of ALIF encode with file storage

--- alif_encode.py
+++ alif_encode.py
@@ -1,61 +1,96 @@
+import os
+import h5py
 from read_dataset import NinaProDB8Lite
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.ndimage import gaussian_filter1d
 import scipy.signal
+from tqdm import tqdm
 
-train_set = NinaProDB8Lite("dataset")
 
-sampling_frequency = 2000
+def main():
+    encode("dataset", True)
+    encode("dataset", False)
 
-for i, (emg, glove) in enumerate(train_set):
 
-    glove_gaussian_filtered = gaussian_filter1d(glove, sigma=500)
+class NinaProDB8ALIFEncoded:
+    def __init__(self, path, train=True):
+        self.path = os.path.join(path, "train_spikes.h5" if train else "test_spikes.h5")
+        with h5py.File(self.path, "r") as f:
+            self.ids = list(f.keys())
 
-    # Remove plateaus
-    plateaus = np.isclose(np.diff(glove_gaussian_filtered), 0, atol=1e-3)
-    emg = emg[:-1, :][~plateaus, :]
-    glove_gaussian_filtered = glove_gaussian_filtered[:-1][~plateaus]
+    def __len__(self):
+        return len(self.ids)
 
-    # Convert glove to uint8
-    glove_gaussian_filtered = glove_gaussian_filtered / glove_gaussian_filtered.max() * 255
-    glove_gaussian_filtered = glove_gaussian_filtered.astype(np.uint8)
+    def __getitem__(self, id):
+        with h5py.File(self.path, "r") as f:
+            grp = f[self.ids[id]]
+            return grp["spikes"][:], grp["glove"][:]
 
-    env_emg = np.abs(scipy.signal.hilbert(emg, axis=0))
 
-    emg_duration, nb_channels = emg.shape
+def encode(path, train):
+    sampling_frequency = 2000
+    data_set = NinaProDB8Lite(path, train=train)
+    type_str = "train" if train else "test"
+    target = h5py.File(os.path.join(path, f"{type_str}_spikes.h5"), "w")
 
-    # Custom a-LIF encoding
-    mem_pot = np.zeros(nb_channels)
-    dt = 1 / sampling_frequency
-    tau = 20e-3
+    for i, (emg, glove) in enumerate(tqdm(data_set)):
 
-    th_rest = np.max(env_emg)
-    th = th_rest * np.ones(nb_channels)
+        glove_gaussian_filtered = gaussian_filter1d(glove, sigma=500)
 
-    th_inc = env_emg.mean()
-    th_tau = 0.500
+        # Best attempt at removing plateaus
+        plateaus = np.isclose(np.diff(glove_gaussian_filtered), 0, atol=1e-3)
+        emg = emg[:-1, :][~plateaus, :]
+        glove_gaussian_filtered = glove_gaussian_filtered[:-1][~plateaus]
 
-    spikes = np.empty((emg_duration, nb_channels), dtype=np.uint8)
+        # Convert glove to uint8
+        glove_gaussian_filtered = glove_gaussian_filtered / glove_gaussian_filtered.max() * 255
+        glove_gaussian_filtered = glove_gaussian_filtered.astype(np.uint8)
 
-    cst = np.exp(-dt / tau)
-    cst_th = np.exp(-dt / th_tau)
+        env_emg = np.abs(scipy.signal.hilbert(emg, axis=0))
 
-    # Go through the data once to figure out the threshold with A-LIF model
-    for t in range(emg_duration):
-        mem_pot = mem_pot * cst + env_emg[t]
-        th = (th - th_rest) * cst + th_rest
-        spikes[t] = mem_pot > th
-        mem_pot *= 1 - spikes[t]
-        th += spikes[t] * th_inc
+        emg_duration, nb_channels = emg.shape
 
-    # Use fixed threshold to actually encode the data with LIF model
-    for t in range(emg_duration):
-        mem_pot = mem_pot * cst + env_emg[t]
-        spikes[t] = mem_pot > th
-        mem_pot *= 1 - spikes[t]
+        # Custom a-LIF encoding
+        mem_pot = np.zeros(nb_channels)
+        dt = 1 / sampling_frequency
+        tau = 20e-3
 
-    fig, axs = plt.subplots(2, sharex=True)
-    axs[0].eventplot([np.flatnonzero(i) for i in spikes.T])
-    axs[1].plot(glove_gaussian_filtered)
-    plt.show()
+        th_rest = np.max(env_emg)
+        th = th_rest * np.ones(nb_channels)
+
+        th_inc = env_emg.mean()
+        th_tau = 0.500
+
+        spikes = np.empty((emg_duration, nb_channels), dtype=np.uint8)
+
+        cst = np.exp(-dt / tau)
+        cst_th = np.exp(-dt / th_tau)
+
+        # Go through the data once to figure out the threshold with A-LIF model
+        for t in range(emg_duration):
+            mem_pot = mem_pot * cst + env_emg[t]
+            th = (th - th_rest) * cst + th_rest
+            spikes[t] = mem_pot > th
+            mem_pot *= 1 - spikes[t]
+            th += spikes[t] * th_inc
+
+        # Use fixed threshold to actually encode the data with LIF model
+        for t in range(emg_duration):
+            mem_pot = mem_pot * cst + env_emg[t]
+            spikes[t] = mem_pot > th
+            mem_pot *= 1 - spikes[t]
+
+        grp = target.create_group(data_set.ids[i])
+        grp["spikes"] = spikes
+        grp["glove"] = glove_gaussian_filtered
+
+        if show_plots := False:
+            fig, axs = plt.subplots(2, sharex=True)
+            axs[0].eventplot([np.flatnonzero(i) for i in spikes.T])
+            axs[1].plot(glove_gaussian_filtered)
+            plt.show()
+
+
+if __name__ == "__main__":
+    main()
